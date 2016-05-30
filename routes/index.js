8a1925e74e8863851cab0722b265696896d13229
aredondor <a.redondo595@gmail.com>
2016-05-30 20:59:22 +0200

    Tema 20: Gestion de Usuarios

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var userController = require('../controllers/user_controller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 res.render('index');
@@ -9,6 +10,7 @@ res.render('index');
 
 //Autoload de rutas que usen :quizId
 router.param('quizId', quizController.load);
+router.param('userId', userController.load);
 // Definición de rutas de /quizzes
 //router.get('/quizzes', quizController.index);
 //router.get('/quizzes/:quizId(\\d+)', quizController.show);
@@ -24,5 +26,15 @@ router.delete('/quizzes/:quizId(\\d+)', quizController.destroy);
 
 router.get('/quizzes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizzes/:quizId(\\d+)/comments', commentController.create);
+
+// Definición de rutas de cuenta
+router.get('/users', userController.index); // listado usuarios
+router.get('/users/:userId(\\d+)', userController.show); // ver un usuario
+router.get('/users/new', userController.new); // formulario sign un
+router.post('/users', userController.create); // registrar usuario
+router.get('/users/:userId(\\d+)/edit', userController.edit); // editar información de cuenta
+router.put('/users/:userId(\\d+)', userController.update); // actualizar información de cuenta
+router.delete('/users/:userId(\\d+)', userController.destroy); // borrar cuenta
+
 module.exports = router;
 
